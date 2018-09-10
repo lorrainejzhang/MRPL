8370b23e09e0d083d6d8eef30e04ab07bb46c91e
lorrainejzhang <37168644+lorrainejzhang@users.noreply.github.com>
2018-09-10 14:25:01 -0400

    Update lab2.m

--- lab2.m
+++ lab2.m
@@ -9,6 +9,9 @@ go = true;
 minRange = 1;
 minx = 5;
 miny = 5;
+
+figure(1); clf;
+
 while(go)
     ranges = robot.laser.LatestMessage.Ranges;
     pause(0.5);
@@ -20,30 +23,30 @@ while(go)
                 minRange = ranges(i);
                 minx = x;
                 miny = y;
-                %plot(minx, miny, 'x');
+                axis([-2 2 -2 2]);
+                plot(miny, -minx, 'x');
+                axis([-2 2 -2 2]);
             end
-            %disp(minRange)
 %             xArray = [xArray, x];
 %             yArray = [yArray, y];
 %             thArray = [thArray, th];
         end
     end    
-    if minRange == 1
+    
+    pause(0.05);
+    
+    if minRange >= 1
         robot.sendVelocity(0,0);
     else
         gain = minRange - .5;
         prop = .5;
-        disp(minRange)
-        disp(prop*gain)
-        robot.sendVelocity(prop*gain,prop*gain);
+        vel = prop*gain;
+        curv = (miny / (minRange * minRange));
+        angVel = curv * vel;
+        tread = 0.085;
+        rghtVel = vel + (tread/2)*curv;
+        leftVel = vel - (tread/2)*curv;
+        robot.sendVelocity(leftVel,rghtVel);
     end
-        
-          
-           
-        
-        %figure(1); clf;
-%         disp(minx)
-%         disp(miny)
-        %plot(minx, miny, 'x');
-    
+   
 end
