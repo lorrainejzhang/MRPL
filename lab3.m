3c1ab9292e4bdb3337180c2d6b85e61db6a04400
lorrainejzhang <37168644+lorrainejzhang@users.noreply.github.com>
2018-09-15 17:06:13 -0400

    Update lab3.m

--- lab3.m
+++ lab3.m
@@ -1,43 +1,18 @@
-robot = raspbot();
-global changed;
-go = true;
+t = 0;
 
-leftArray = zeros(1,1);
-rghtArray = zeros(1,1);
+% leftArray = zeros(1,1);
+% rghtArray = zeros(1,1);
+posx = 0;
+posy = 0;
 
-while go
-    
-    robot.sendVelocity(0.2, 0.2);
-    
-    leftEncoder = robot.encoders.LatestMessage.Vector.X;
-    rghtEncoder = robot.encoders.LatestMessage.Vector.Y;
-    time = tic;
-    
-    
-    changed = 0;
-    
-    while(changed == 0)
-        robot.encoders.NewMessageFcn=@encoderEventListener;
-        pause(.1);
-        disp("here")
-    end
-
-    % differentiate left and right wheel encoders
-    
-    timediff = toc(tic);
-    leftEncoderNew = robot.encoders.LatestMessage.Vector.X;
-    rghtEncoderNew = robot.encoders.LatestMessage.Vector.Y;
-    
-    leftVel = (leftEncoderNew - leftEncoder) / timediff;
-    rghtVel = (rghtEncoderNew - rghtEncoder) / timediff;
-    disp(leftVel)
-    
-    leftArray = [leftArray, leftVel];
-    rghtArray = [rghtArray, rghtVel];
-    
-    changed = 0;
-    
-    plot(rghtArray);
-    
-    pause(0.5);
+while(t <= sqrt(32 * pi))
+    v = 0.1;
+    k = 1/8;
+    tread = 0.085;
+    angVel = k * t;
+    vr = v + (tread / 2) * angVel;
+    vl = v - (tread / 2) * angVel;
+    dt = 0.001;
+    [x, y, th] = modelDiffSteerRobot(vl, vr, t, t+dt*10, dt);
+    t = t + dt;
 end
