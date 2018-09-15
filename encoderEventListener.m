63e23aeed6031da4a3fffc31f4255042168b7223
s-hall <42918002+s-hall@users.noreply.github.com>
2018-09-15 15:29:05 -0400

    Update encoderEventListener.m

--- encoderEventListener.m
+++ encoderEventListener.m
@@ -1,6 +1,10 @@
 function encoderEventListener(handle,event)
-    persistent changed
-    encoderDataTimestamp = double(event.Header.Stamp.Sec) + double(event.Header.Stamp.Nsec)/1000000000.0;
-    % encoderFrame = encoderFrame + 1;
+    global changed;
     changed = 1;
+    encoderDataTimestamp = double(event.Header.Stamp.Sec) + double(event.Header.Stamp.Nsec)/1000000000.0;
+    %disp("hi")
+    % encoderFrame = encoderFrame + 1;
+    
+  
+    
 end
