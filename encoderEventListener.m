821e10062233fb5cf9f771f100dfa30611b99bb8
Steven Hall <smhall@andrew.cmu.edu>
2018-09-17 23:03:10 -0400

    yay

--- encoderEventListener.m
+++ encoderEventListener.m
@@ -1,10 +1,37 @@
 function encoderEventListener(handle,event)
-    global changed;
-    changed = 1;
+    global enposx;
+    global enposy;
+    global enposth
+    global oldLeft;
+    global oldRght;
+    global oldt;
+    global robot;
+    global leftFirst;
+    global rghtFirst;
+    tread = 0.085;
     encoderDataTimestamp = double(event.Header.Stamp.Sec) + double(event.Header.Stamp.Nsec)/1000000000.0;
-    %disp("hi")
-    % encoderFrame = encoderFrame + 1;
+    dt = encoderDataTimestamp - oldt;
+    oldt = encoderDataTimestamp;
     
-  
+    dleft = robot.encoders.LatestMessage.Vector.X - leftFirst - oldLeft;
+    drght = robot.encoders.LatestMessage.Vector.Y - rghtFirst - oldRght;
+    oldLeft = robot.encoders.LatestMessage.Vector.X - leftFirst;
+    oldRght = robot.encoders.LatestMessage.Vector.Y - rghtFirst;
+    envl = dleft/dt; 
+    envr = drght/dt;
+    env = (envl+envr)/2;
+    enangVel = (envr-envl)/tread;
+    ds = env*dt;
+    endth = enangVel*dt/2;
+    enposth = enposth + endth;
+    endx = cos(enposth)*ds;
+    endy = sin(enposth)*ds;
+    enposth = enposth + endth;
+    
+    
+    enposx = enposx + endx;
+    enposy = enposy + endy;
+    
+    pause(.05);
     
 end
--- f8.m
+++ f8.m
@@ -1,5 +1,34 @@
+global enposx;
+global enposy;
+global enposth
+global oldLeft;
+global oldRght;
+global oldt;
+global robot;
+global leftFirst;
+global rghtFirst;
+
+enposx = 0.0;
+enposy = 0.0;
+enposth = 0.0;
+oldLeft = 0.0;
+oldRght = 0.0;
+oldt = 0.0;
+robot = raspbot();
+robot.encoders.NewMessageFcn=@encoderEventListener;
+leftFirst = robot.encoders.LatestMessage.Vector.X;
+rghtFirst = robot.encoders.LatestMessage.Vector.Y;
+
 xArray = zeros(1,300000);
 yArray = zeros(1,300000);
+
+xenArray = zeros(1, 600000);
+yenArray = zeros(1, 600000);
+% myPlot = plot(xenArray, yenArray, 'b-');
+% xlim([-.6 .6]);
+% ylim([-.6 .6]);
+
+
 
 v = 0.2;
 sf = 1;
@@ -7,12 +36,15 @@ tf = sf/v;
 kth = 2*pi/sf;
 kk = 15.1084;
 
-ks = 1;
+ks = 3;
 Tf = ks*tf;
 tread = 0.085;
 posx = 0;
 posy = 0;
 posth = 0;
+posenx = 0;
+poseny = 0;
+posenth = 0;
 
 i = 0;
 told = 0;
@@ -29,21 +61,66 @@ while(T <= Tf)
     angVel = curv*v;
     vr = v + (tread / 2) * angVel;
     vl = v - (tread / 2) * angVel;
-    
-    [dx, dy, dth] = modelDiffSteerRobot(vl, vr, told, t, (t - told)/10); % local trajectory est.
-    r = sqrt(dx^2 + dy^2); % have to change it from robot coords to our plot
-    posth = posth + dth; % since it changed direction
-    posx = posx + cos(posth)*r;
-    posy = posy + sin(posth)*r;
+    robot.sendVelocity(vl, vr);
     
     i = i + 1;
-    xArray(i) = posx;
-    yArray(i) = posy;
-end
+    xenArray(i) = enposx;
+    yenArray(i) = enposy;
+    pause(.05);
+    
+    %leftOld = robot.encoders.LatestMessage.Vector.X;
+    %rghtOld = robot.encoders.LatestMessage.Vector.Y;
+    %time = tic;
+    
+    
+%     changed = 0;
+%     
+%     while(changed == 0)
+%         
+%         pause(.1);
+%         %disp("here")
+%     end
 
-plot(xArray(1, 1:i), yArray(1, 1:i));
-% xlim([0 0.4]);
-% ylim([0 0.4]);
+    % differentiate left and right wheel encoders
+        
+%     [endx, endy, endth] = modelDiffSteerRobot(envl, envr, t-timediff, t, timediff/10); % local trajectory est.
+%     enr = sqrt(endx^2 + endy^2); % have to change it from robot coords to our plot
+%     posenth = posenth + endth; % since it changed direction
+%     posenx = posenx + cos(posenth)*enr;
+%     poseny = poseny + sin(posenth)*enr;
+    
+    
+%     plot(xenArray(1, 1:i), yenArray(1, 1:i));
+%     xlim([-.6 .6]);
+%     ylim([-.6 .6]);
+    %set(myPlot, 'xdata', [get(myPlot,'xdata') xenArray], 'ydata', [get(myPlot,'ydata') yenArray]);
+    
+    %plot(xenArray(1:i
+    
+%     [dx, dy, dth] = modelDiffSteerRobot(vl, vr, told, t, (t - told)/10); % local trajectory est.
+%     r = sqrt(dx^2 + dy^2); % have to change it from robot coords to our plot
+%     posth = posth + dth; % since it changed direction
+%     posx = posx + cos(posth)*r;
+%     posy = posy + sin(posth)*r;
+%     
+%     %i = i + 1;
+%     xArray(i) = posx;
+%     yArray(i) = posy;
+    
+   
+    
+    
+end
+robot.sendVelocity(0,0);
+disp(i)
+plot(xenArray(1, 3:i), yenArray(1, 3:i));
+xlim([-.6 .6]);
+ylim([-.6 .6]);
+
+%plot(xenArray(1, 1:i), yenArray(1, 1:i));
+%plot(xArray(1, 1:i), yArray(1, 1:i));
+% xlim([-.6 .6]);
+% ylim([-.6 .6]);
 % title('cornu');
 % xlabel('x');
 % ylabel('y');
