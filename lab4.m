22f72566755eac1fa1c20adb7688ab03bd4b204a
Steven Hall <smhall@andrew.cmu.edu>
2018-09-23 23:11:57 -0400

    trapezoid vel

--- lab4.m
+++ lab4.m
@@ -8,12 +8,15 @@ timeArray = zeros(1,1);
 leftArray = zeros(1,1);
 rightArray = zeros(1,1);
 errorArray = zeros(1,1);
+urefArray = zeros(1,1);
+srefArray = zeros(1,1);
+posArray = zeros(1,1);
 
-plot(timeArray, leftArray, timeArray, rightArray)
-title('Lab1')
-xlabel('Time (s)')
-ylabel('Distance (m)')
-legend('Left Wheel', 'Right Wheel');
+% plot(timeArray, leftArray, timeArray, rightArray)
+% title('Lab1')
+% xlabel('Time (s)')
+% ylabel('Distance (m)')
+% legend('Left Wheel', 'Right Wheel');
 
 kp = 10.0;
 kd = 0.03;
@@ -24,11 +27,12 @@ error = 0;
 time = 0;
 oldTime = 0;
 errorIntegral = 0;
+sref = 0;
 
 pos = 0;
 tic
 
-while(0.1 - pos >= 0.0001 && time < 6)
+while((1 - abs(pos)) >= 0.0001 && time < 6)
     oldError = error;
     oldTime = time;
     time = toc;
@@ -39,26 +43,37 @@ while(0.1 - pos >= 0.0001 && time < 6)
     leftPos = robot.encoders.LatestMessage.Vector.X;
     rightPos = robot.encoders.LatestMessage.Vector.Y;
     pos = (((leftPos + rightPos)/2) - avgStart);
+    %disp(pos);
+    
+    delTime = time - oldTime;
+    uref = trapezoidalVelocityProfile(time,1);
+    sref = sref + (uref * delTime);
+    %disp(sref);
     
     error = exPos - pos;
     errorArray = [errorArray, error];
+    urefArray = [urefArray, uref];
+    srefArray = [srefArray, sref];
+    posArray = [posArray, pos];
     
-    delTime = time - oldTime;
+    
+    
     
     errorDerivative = (error - oldError) / delTime;
     errorIntegral = errorIntegral + error * delTime;
-    control = error*kp + errorDerivative*kd + errorIntegral*ki;
-    sendVelocity(robot, .03 + control, .03 + control);
+    %control = error*kp + errorDerivative*kd + errorIntegral*ki;
+    control = uref;
+    sendVelocity(robot, control, control);
     
     timeArray = [timeArray, time];
     rightArray = [rightArray, rightPos - rightStart];
     leftArray = [leftArray, leftPos - leftStart];
     
-    plot(timeArray, errorArray);
+    %plot(timeArray, urefArray);
     title('Lab1');
     xlabel('Time (s)');
     ylabel('Error');
 end
 
-
+plot(timeArray, srefArray, timeArray, posArray);
 robot.stop();
--- trapezoidalVelocityProfile.m
+++ trapezoidalVelocityProfile.m
@@ -1,4 +1,4 @@
-function uref = trapezoidalVelocityProfile( t , amax, vmax, dist, sgn)
+function uref = trapezoidalVelocityProfile(t, sgn)
 %uref Return the velocity command of a trapezoidal profile.
 % Returns the velocity command of a trapezoidal profile of maximum
 % acceleration amax and maximum velocity vmax whose ramps are of
@@ -7,11 +7,17 @@ function uref = trapezoidalVelocityProfile( t , amax, vmax, dist, sgn)
 vmax = 0.25;
 amax = 3 * 0.25;
 tramp = vmax/amax;
+sf = 1;
 tf = (sf + (vmax^2) / amax) / vmax;
 if(t < tramp)
-    robot.sendVelocity(amax * t, amax * t);
-else if(t > tramp && t < tf)
-     robot.sendVelocity(vmax, vmax);
-else if ((tf - t) < tramp)
-     robot.sendVelocity(amax * (tf - t);
+    uref = sgn * amax * t;
+elseif (t > tramp && t < tf)
+    uref = sgn * vmax;
+elseif ((tf - t) < tramp)
+    uref = sgn * amax * (tf - t);
+elseif ((tf - t) < 0)
+    uref = 0;
+else
+    uref = 0;
+end
 end
