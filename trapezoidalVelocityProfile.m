7afe260e709b2e35c25e6f6177baf8db2c5a3433
geejoseph <42917809+geejoseph@users.noreply.github.com>
2018-09-25 01:40:10 -0400

    Something maybe still wrong??

--- trapezoidalVelocityProfile.m
+++ trapezoidalVelocityProfile.m
@@ -6,19 +6,18 @@ function uref = trapezoidalVelocityProfile(t, sgn)
 % Returns 0 if t is negative. 
 vmax = 0.25;
 amax = 3 * 0.25;
-tramp = vmax/amax;
+tramp = vmax/amax; %1/3 meter
+tdelay = 0.2125;
 sf = 1;
 tf = (sf + (vmax^2) / amax) / vmax;
-if (t < 0)
+if (t < 0 || t > tf)
     uref = 0;
 elseif (t < tramp)
     uref = sgn * amax * t;
-elseif (t > tramp && t < tf)
-    uref = sgn * vmax;
-elseif ((tf - t) < tramp)
+elseif (tf - t < tramp)
     uref = sgn * amax * (tf - t);
-elseif ((tf - t) < 0)
-    uref = 0;
+elseif (tramp < t && t < tf - tramp)
+    uref = sgn * vmax;
 else
     uref = 0;
 end
