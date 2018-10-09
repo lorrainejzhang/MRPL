eb4b2bbbeedbfc12418d195062353606c6870bbc
geejoseph <42917809+geejoseph@users.noreply.github.com>
2018-10-08 21:50:31 -0400

    Update trajectoryFollower for OOP!

--- trajectoryFollower.m
+++ trajectoryFollower.m
@@ -1,32 +1,32 @@
 classdef trajectoryFollower
     methods
-        function [] = sendVel(obj, robot, T, traj, enposx,enposy,enposth, goodT, ref)
+        function [] = sendVel(obj, robot, T, traj, enposx,enposy,enposth, goodT, feedbackOn)
             tread = 0.085;
-            V = traj.getVelAtTime(T);
-            w = traj.getOmegaAtTime(T);
+            V = traj.getVAtTime(T);
+            w = traj.getwAtTime(T);
             %disp(enposx)
             cont = controller();
             %backV = 0; backW = 0;
             [backV, backW] = cont.feedback(T, traj, enposx,enposy,enposth, V);
 %             disp(backV)
 %             disp(backW)
+            backV = feedbackOn * backV;
+            backW = feedbackOn * backW;
             vr = (V + backV) + (tread / 2) * (w + backW);
             vl = (V + backV) - (tread / 2) * (w + backW);
             
             if isnan(vr) || isinf(vr)
-                %disp('gs')
                 vr = 0;
             end
             if isnan(vl) || isinf(vl)
-                %disp('sds')
                 vl = 0;
             end
             if (T > ref.getTrajectoryDuration) && (T < ref.getTrajectoryDuration + 2)
                 vl = 0;
                 vr = 0;
             end
-%             disp(vl)
-%             disp(vr)
+            %disp(vl)
+            %disp(vr)
 
             robot.sendVelocity(vl,vr);
         end
