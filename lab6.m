df3caebef5b25647f75a70449124535201d04006
lorrainejzhang <37168644+lorrainejzhang@users.noreply.github.com>
2018-10-10 22:38:42 -0400

    Create lab6.m

---
+++ lab6.m
@@ -0,0 +1,8 @@
+figure(1);
+mrplSystem.executeTrajectoryToRelativePose(0.3048, 0.3048, 0, 0.2, 0);
+pause(0.05);
+figure(2);
+mrplSystem.executeTrajectoryToRelativePose(-0.6096, -0.6096, -pi()/2.0, 0.2, 0);
+pause(0.05);
+figure(3);
+mrplSystem.executeTrajectoryToRelativePose(-0.3048, 0.3048, pi()/2.0, 0.2, 0);
