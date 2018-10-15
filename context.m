aa97f478f03e4420a2c5e174fe3088857867e577
Steven Hall <smhall@andrew.cmu.edu>
2018-10-15 18:28:29 -0400

    lab7

---
+++ context.m
@@ -0,0 +1,19 @@
+classdef context < handle
+    properties
+        ti;
+        robot;
+        goodT;
+        maxV = .2;
+        feedbackOn = 0;
+    end
+    methods
+        function obj = context()
+            obj.ti = tic();
+            obj.robot = raspbot();
+        end
+        
+        function T = getLocalTime(obj)
+            T = toc(obj.ti);
+        end
+    end
+end
